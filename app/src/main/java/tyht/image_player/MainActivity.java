6769c23987992916aa461a924d52e1c089140aad
liangchangwei <sam_2008h@126.com>
2019-11-14 21:19:55 +0800

    add write file code

--- app/src/main/java/tyht/image_player/MainActivity.java
+++ app/src/main/java/tyht/image_player/MainActivity.java
@@ -1,32 +1,33 @@
 package tyht.image_player;
 
+import android.support.v7.app.AppCompatActivity;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import tyht.image_player.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "newplayer";
     String[] Images = null;
     AssetManager assets = null;
     int currentImg=0;
     ImageView image;
     TextView text;
     Button button;
-
+    ArrayList<String> RGBfilelist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         image = (ImageView)findViewById(R.id.image);
         text = (TextView)findViewById(R.id.text);
         button = (Button)findViewById(R.id.button);
+        RGBfilelist = new ArrayList<>();
+        Log.i(TAG," onCreate ");
+        Utils.removeFile("/sdcard/Android_Image/filelist.txt");
 
         try{
             assets = getAssets();
@@ -43,7 +47,17 @@ public class MainActivity extends AppCompatActivity {
             String filelist = "filelist.txt";
             for(int i = 0; i < Images.length; i++){
                 String filename = Images[i];
-                Utils.writeTxtToFile(filename, Utils.getfilepath(), filelist);
+                Log.i(TAG," filename" + i + " = " + filename );
+                if(filename.endsWith(".jpg")){
+                    String newfilename = filename.substring(0, filename.indexOf(".jpg"));
+                    Log.i(TAG," newfilename = " + newfilename);
+                    Utils.writeTxtToFile(newfilename, Utils.getfilepath(), filelist);
+                    RGBfilelist.add(newfilename);
+                    InputStream srcInput = assets.open(filename);
+                    Bitmap srcBitmap = BitmapFactory.decodeStream(srcInput,null, new BitmapFactory.Options());
+                    Utils.processFile(Utils.comp(srcBitmap), newfilename);
+                }
+                //Utils.
             }
         }catch (IOException e){
             e.printStackTrace();
@@ -74,16 +88,22 @@ public class MainActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    Log.i(TAG,"onClick");
                     Bitmap outbitmap = Utils.comp(finalbitmap);
-
                     String S = "原始图片大小为：" + finalbitmap.getWidth() + "X" + finalbitmap.getHeight();
                     S += " 缩放后大小为：" + outbitmap.getWidth() + "X" + outbitmap.getHeight() ;
                     text.setText(S);
                     image.setImageBitmap(outbitmap);
+                    //Utils.ReadTxtFile("/sdcard/Android_Image/filelist.txt");
+//                    for(int i = 0 ; i < RGBfilelist.size(); i++ ){
+//                        String filename = Utils.getfilepath() + RGBfilelist.get(i);
+//                        Log.i(TAG,"filename = " + filename);
+//                        Utils.ReadTxtFile(filename);
+//                    }
+//                    Log.i(TAG,"ReadTextFile end ");
                 }
             });
         }
+        Log.i(TAG," OnCreate end ");
     }
-
-
 }
--- app/src/main/java/tyht/image_player/utils/Utils.java
+++ app/src/main/java/tyht/image_player/utils/Utils.java
@@ -2,11 +2,17 @@ package tyht.image_player.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 
 /**
@@ -14,9 +20,12 @@ import java.io.RandomAccessFile;
  */
 
 public class Utils {
-    private static String file_path = "/storage/emulated/0/image_player/";
+    //private static String file_path = "/storage/emulated/0/image_player/";
+    private static final String TAG = "newplayer";
 
     public static String getfilepath(){
+        String file_path = "/sdcard/AndroidImage/"; //Environment.getExternalStorageDirectory().getAbsolutePath();
+        //file_path = file_path + "/Android/";
         return file_path;
     }
 
@@ -81,10 +90,12 @@ public class Utils {
     // 将字符串写入到文本文件中
     public static void writeTxtToFile(String strcontent, String filePath, String fileName) {
         //生成文件夹之后，再生成文件，不然会出错
+        //Log.i(TAG," begin writeTxtToFile strcontent = " + strcontent );
         makeFilePath(filePath, fileName);
 
         String strFilePath = filePath + fileName;
         // 每次写入时，都换行写
+        //Log.i(TAG," writeTxtToFile strFilePath = " + strFilePath );
         String strContent = strcontent + "\r\n";
         try {
             File file = new File(strFilePath);
@@ -96,12 +107,14 @@ public class Utils {
             RandomAccessFile raf = new RandomAccessFile(file, "rwd");
             raf.seek(file.length());
             raf.write(strContent.getBytes());
+            //Log.i(TAG,"write end");
             raf.close();
         } catch (Exception e) {
-            Log.e("TestFile", "Error on write File:" + e);
+            Log.e(TAG, "Error on write File:" + e);
         }
     }
     private static File makeFilePath(String filePath, String fileName) {
+        //Log.i(TAG,"begin makeFilePath");
         File file = null;
         makeRootDirectory(filePath);
         try {
@@ -112,6 +125,7 @@ public class Utils {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        //Log.i(TAG,"makeFilePath file = " + file);
         return file;
     }
 
@@ -119,13 +133,112 @@ public class Utils {
 
     private static void makeRootDirectory(String filePath) {
         File file = null;
+        //Log.i(TAG,"makeRootDirectory filePath = " + filePath);
         try {
             file = new File(filePath);
             if (!file.exists()) {
                 file.mkdir();
             }
         } catch (Exception e) {
-            Log.i("error:", e + "");
+            Log.i(TAG, e + "");
         }
+    }
+
+    public static String ReadTxtFile(String strFilePath)
+    {
+        String path = strFilePath;
+        String content = ""; //文件内容字符串
+
+        Log.i(TAG,"ReadTxtFile strFilePath = " + strFilePath);
+        //打开文件
+        File file = new File(path);
+        //如果path是传递过来的参数，可以做一个非目录的判断
+        if (file.isDirectory())
+        {
+            Log.d(TAG, "The File doesn't not exist.");
+        }
+        else
+        {
+            try {
+                InputStream instream = new FileInputStream(file);
+                if (instream != null)
+                {
+                    InputStreamReader inputreader = new InputStreamReader(instream);
+                    BufferedReader buffreader = new BufferedReader(inputreader);
+                    String line;
+                    //分行读取
+                    while (( line = buffreader.readLine()) != null) {
+                        content += line + "\n";
+                    }
+                    instream.close();
+                }
+            }
+            catch (java.io.FileNotFoundException e)
+            {
+                Log.d(TAG, "The File doesn't not exist.");
+            }
+            catch (IOException e)
+            {
+                Log.d(TAG, e.getMessage());
+            }
+        }
+        //Log.i(TAG,"ReadTxtFile content = " + content);
+        return content;
+    }
+
+    public static void removeFile(String strFilePath) {
+        String path = strFilePath;
+        File file = new File(path);
+        Log.i(TAG,"file is exists = " + file.exists() + " begin delete");
+        if(file.exists()&& file.isDirectory() == false){
+            file.delete();
+        }
+        Log.i(TAG,"after delete file is exists = " + file.exists());
+    }
+
+    public static void processFile(Bitmap bitmap, String newfilename) {
+        String store_filepath = getfilepath() + newfilename;
+        removeFile(store_filepath);
+        Log.i(TAG,"begin processFile newfilename = " + newfilename);
+        Log.i(TAG,"bitmap width = " + bitmap.getWidth() + " height = " + bitmap.getHeight());
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+        int r = 0;
+        if(w > h ){
+            r = (int)(h/2);
+        }else{
+            r = (int)(w/2);
+        }
+        Log.i(TAG," processFile r = " + r);
+        int start_x = (int)(w/2);
+        int start_y = (int)(h/2);
+        Log.i(TAG,"start_x = " + start_x + " start_y = " + start_y);
+
+        for(int i = 0; i< 360; i++){
+            int line = i+1;
+            String temp_log = "";
+            String red_line = "" + line +" red =";
+            String green_line = "" + line + " green = ";
+            String blue_line = "" + line + " blue = ";
+            for(int j = 0; j < r; j ++){
+                int temp_y = (int)(start_y + j * Math.sin(2*Math.PI*i/360));//从中心点竖直往下为第一帧，逆时针转
+                int temp_x = (int)(start_x + j * Math.cos(2*Math.PI*i/360));
+                int color = bitmap.getPixel(temp_x, temp_y);
+                String red = Integer.toHexString(Color.red(color));
+                String green = Integer.toHexString(Color.green(color));
+                String blue = Integer.toHexString(Color.blue(color));
+                red_line += red;
+                green_line += green;
+                blue_line += blue;
+                temp_log += " (" + temp_x + "," + temp_y +") = (" + red + "," + green + "," + blue + ") ";
+            }
+            writeTxtToFile(red_line, getfilepath(),newfilename);
+            writeTxtToFile(green_line, getfilepath(),newfilename);
+            writeTxtToFile(blue_line, getfilepath(),newfilename);
+            Log.i(TAG," red_line = " + red_line);
+            Log.i(TAG," green_line = " + green_line);
+            Log.i(TAG," blue_line = " + blue_line);
+        }
+
     }
 }
